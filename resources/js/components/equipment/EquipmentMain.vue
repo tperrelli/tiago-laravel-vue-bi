67f6f3592d14ca8f6557c4f8dcaef6e6d66c8585
Tiago Perrelli <tiagoyg@gmail.com>
2024-08-12 16:36:01 -0300

    added months to select

--- resources/js/components/equipment/EquipmentMain.vue
+++ resources/js/components/equipment/EquipmentMain.vue
@@ -6,18 +6,20 @@
         <form action="">
           <div class="row row-cols-lg-auto">
             <div class="col-12">
-              <select class="form-select">
-                  <option value="{{ coin }}" v-for="(coin, index) in coins">{{  coin }}</option>
+              <select class="form-select" v-model="form.symbol">
+                  <option :value="coin" v-for="(coin, index) in coins">{{  coin }}</option>
               </select>
             </div>
             <div class="col-12">
-              <input type="text" class="form-control" placeholder="Name" aria-label="Name">
+              <input type="text" class="form-control" placeholder="Name" aria-label="Name" v-model="form.name" />
             </div>
             <div class="col-12">
-              <input type="text" class="form-control" placeholder="Amount" aria-label="Amount">
+              <input type="text" class="form-control" placeholder="Amount" aria-label="Amount" v-model="form.amount" />
             </div>
             <div class="col-12">
-              <input type="text" class="form-control" placeholder="Month" aria-label="Month">
+              <select class="form-select" v-model="form.month">
+                <option :value="month.value" v-for="(month, index) in months">{{ month.label }}</option>
+              </select>
             </div>
             <div class="col-12">
               <input type="button" @click="filterData" class="btn btn-success" value="Filter" />
@@ -42,16 +44,17 @@ const mainChart = ref(null);
 
 const coins = ref(['- all -', 'BTC', 'ETH', 'SOL']);
 const labels = ref(['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun']);
+const months = ref([
+  { label: 'Jan', value: 1 }, { label: 'Fev', value: 2 }, { label: 'Mar', value: 3 }, { label: 'Apr', value: 4 }, { label: 'Mai', value: 5 }, { label: 'Jun', value: 6 }
+])
+
 const datasets = ref([]);
 
-const chartData = {
-  labels: labels.value,
-  datasets: datasets.value
-};
-
-const chartOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
+const form = {
+  symbol: null,
+  name: null,
+  amount: null,
+  month: null
 };
 
 const loadData = async () => {
@@ -73,20 +76,47 @@ const loadData = async () => {
  
 const loadChart = () => {
   if (mainChart.value) {
+
+    const chartOptions = {
+      responsive: true,
+      maintainAspectRatio: false,
+    };
+
     new Chart(mainChart.value, {
       type: 'bar', 
-      data: chartData, 
+      data: {
+        labels: labels.value,
+        datasets: datasets.value
+      }, 
       options: chartOptions
     });
   }
 };
 
-const filterData = () => {
+const filterData = async () => {
 
-  //TODO
+  const params = {};
+
+  if (form.symbol) {
+    params.symbol = form.symbol;
+  }
+
+  if (form.amount) {
+    params.amount = form.amount;
+  }
+
+  if (form.month) {
+    params.month = form.month;
+  }
+
+  const response = await httpRequest.get('/api/stocks', { params });
+
+  if (response.status === OK) {
+    datasets.value = [];
+    loadData();
+  }
 };
 
 loadData();
-filterData();
 
 </script>
