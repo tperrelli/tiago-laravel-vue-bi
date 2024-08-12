ecff9230ef6036b0b2bfb74af2355a515cc675ee
Tiago Perrelli <tiagoyg@gmail.com>
2024-08-12 16:58:37 -0300

    fixed month criteria

--- app/Repositories/Criterias/MonthCriteria.php
+++ app/Repositories/Criterias/MonthCriteria.php
@@ -10,7 +10,7 @@ class MonthCriteria implements CriteriaInterface
     public function apply(Builder $query, array $filters): Builder
     {
         if (!empty($filters['month'])) {
-            $query->whereRaw('MONTH(created_at)', $filters['month']);
+            $query->whereMonth('created_at', 2);
         }
 
         return $query;
