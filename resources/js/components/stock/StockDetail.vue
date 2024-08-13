2b7838c96422fdc79ff4cc9a8f64cfd554334e87
Tiago Perrelli <tiagoyg@gmail.com>
2024-08-13 05:36:37 -0300

    adding basic structure for stock detail

--- resources/js/components/stock/StockDetail.vue
+++ resources/js/components/stock/StockDetail.vue
@@ -1,3 +1,29 @@
 <template>
-  
+  <div class="card">
+    <div class="card-body">
+      <!-- TODO
+        list
+      -->
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import httpRequest from '../../../Services/Http';
+
+const OK = 200;
+const items = ref([]);
+
+const symbol = ref('');
+const endpoint = `/api/stocks?symbol=${symbol.value}`;
+
+const loadData = async () => {
+  const response = await httpRequest.get(endpoint, );
+  if (response.status === OK) {
+    items.value = response.data.data;
+  }
+};
+
+watch([symbol], loadData);
+</script>
